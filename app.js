2d66df46fc070ed66d664c63fc04f04e9ae1eec1
MariaDamyanova0 <mdamyanova3535@gmail.com>
2026-02-05 17:53:19 +0200

    Add delete button for tasks

--- app.js
+++ app.js
@@ -3,19 +3,31 @@ const addButton = document.querySelector(".task-input button");
 const taskList = document.querySelector(".task-list");
 
 addButton.addEventListener("click", addTask);
+
 input.addEventListener("keydown", (e) => {
   if (e.key === "Enter") addTask();
 });
 
-
 function addTask() {
   const taskText = input.value.trim();
-
   if (taskText === "") return;
 
   const li = document.createElement("li");
-  li.textContent = taskText;
 
+  const span = document.createElement("span");
+  span.textContent = taskText;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "✕";
+  deleteBtn.classList.add("delete-btn");
+
+  deleteBtn.addEventListener("click", () => {
+    li.remove();
+  });
+
+  li.appendChild(span);
+  li.appendChild(deleteBtn);
   taskList.appendChild(li);
+
   input.value = "";
 }
